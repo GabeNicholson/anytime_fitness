ab5b537dde7fd938d88ceed412dba8578bf93321
Gabriel Nicholson <gnicholson@twitter.com>
2024-03-13 10:16:22 -0700

    refactor: add print and disable copy warning

--- daily_email/prepare_dim_email.py
+++ daily_email/prepare_dim_email.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import prepare_email_helpers
+pd.options.mode.chained_assignment = None  # default='warn'
+
 
 load_dotenv()
 
@@ -15,16 +18,7 @@ connection = sql_engine.raw_connection()
 
 complete_df = pd.read_sql("SELECT * FROM Users ORDER BY id", con=connection)
 
-db_backup_directory_path = "../user_db_backup/"
-if not os.path.exists(db_backup_directory_path):
-    print("---MAKING NEW BACKUP DIRECTORY---")
-    os.makedirs(db_backup_directory_path)
-backup_parquets: list[str] = os.listdir(db_backup_directory_path)
-backup_suffix_nums: list[int] = [int(pq_file.split("backup_")[1][0]) for pq_file in backup_parquets]
-if backup_suffix_nums:
-    max_backup_file_suffix = max(backup_suffix_nums) + 1
-else:
-    max_backup_file_suffix = 1
+max_backup_file_suffix: int = prepare_email_helpers.get_backup_file_suffix()
 complete_df.to_parquet(f"../user_db_backup/database_backup_{max_backup_file_suffix}.parquet")
 
 total_signups = len(complete_df)
@@ -44,7 +38,9 @@ print(df_previous)
 
 assert len(new_df) + len(df_previous) <= total_signups
 assert len(new_df) > 0
+
 new_df.to_csv("~/Desktop/dims_leads_previous.csv", index=False)
 
 df_email = new_df[['name', 'email', 'phone', "id"]]
 df_email.to_csv("~/Desktop/dims_leads.csv", index=False)
+print("Writing csv success!")
